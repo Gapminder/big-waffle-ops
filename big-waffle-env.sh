4da6b7dc150f92da9e2e3ad1d157ff26af42d23a
Robert Aarts <robert@lokkilok.com>
2019-03-13 17:54:37 +0200

    Removed Git working dir from common environment.

--- big-waffle-env.sh
+++ big-waffle-env.sh
@@ -2,4 +2,3 @@
 # in the Gapminder BigWaffle deployment on Google Cloud
 export ASSET_STORE_BUCKET="org-gapminder-big-waffle-assets"
 export BIG_WAFFLE_HOME=/gapminders/big-waffle
-export LOAD_GIT_DIR=/gapminders/tmp

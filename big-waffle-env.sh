787a11de28480bed0abd6b1393898dd6ffa5d843
Robert Aarts <robert@lokkilok.com>
2019-03-13 09:43:01 +0200

    Added essential env vars to a bash profile script.

---
+++ big-waffle-env.sh
@@ -0,0 +1,5 @@
+# Environment variables used by the BigWaffle CLI and BigWaffleOps scripts
+# in the Gapminder BigWaffle deployment on Google Cloud
+export ASSET_STORE_BUCKET="org-gapminder-big-waffle-assets"
+export BIG_WAFFLE_HOME=/gapminders/big-waffle
+export LOAD_GIT_DIR=/gapminders/tmp

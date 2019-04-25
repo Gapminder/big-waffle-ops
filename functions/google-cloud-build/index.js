e52bc3326668d4b151a9cc5dd14d4c659780ac2e
Robert Aarts <robert@lokkilok.com>
2019-04-25 12:28:58 +0300

    Added GitHub status updates.

--- functions/google-cloud-build/index.js
+++ functions/google-cloud-build/index.js
@@ -1,3 +1,4 @@
+const fetch = require('node-fetch')
 const { getConfig, exec } = require('bw-cf-utils')
 
 function error(err) {
@@ -13,20 +14,60 @@ const eventToBuild = (data) => {
   return JSON.parse(new Buffer(data, 'base64').toString());
 }
 
+const StatusMap = {
+  QUEUED: 'pending',
+  WORKING: 'pending',
+  SUCCESS: 'success',
+  FAILURE: 'failure',
+  INTERNAL_ERROR: 'error',
+  TIMEOUT: 'error',
+  CANCELLED: 'error'
+}
+
 exports.processEvent =  event => {
-  let cmd   
+  let cmd, updateGitHubStatus 
 
   try {
     const build = eventToBuild(event.data)
     console.debug(JSON.stringify(build))
-    // QUEUED, WORKING, SUCCESS, FAILURE, INTERNAL_ERROR, TIMEOUT, CANCELLED, 'FAILURE', 'INTERNAL_ERROR', 'TIMEOUT'
+    
+    // update commit status with simple POSTs like:
+    let repo, commit, status
+    try {
+      repo = build.source.repoSource.projectId
+      commit = build.sourceProvenance.resolvedRepoSource.commitSha
+      status = StatusMap[build.status]
+    } catch (err) {
+      console.info(err)
+    }
+    if (repo && commit && status) {
+      updateGitHubStatus = (config) => {
+        fetch(`https://api.github.com/repos/Gapminder/${repo}/statuses/${commit}`, {
+          method: 'post',
+          body: JSON.stringify({
+            state: status,
+            target_url: build.logUrl,
+            context: 'ci/google-cloud'
+          }),
+          headers: {
+            'Authorization': `token ${config.GitHubToken}`,
+            'Content-Type': 'application/json'
+          }
+        })
+        .then(res => {
+          if (!res.ok) {
+            console.err('Could not update GitHub status')
+          }
+          return config
+        })
+      }
+    }
+
+    // prepare command to run on master to update the code 
     if (['SUCCESS'].includes(build.status) 
         && build.source.repoSource 
         && build.source.repoSource.branchName === 'production') {
       cmd = 'cd $BIG_WAFFLE_HOME && git pull'
-    }
-    if (!cmd) {
-      return 'No action required'
     }
   } catch (err) {
     console.debug(event.data)
@@ -36,8 +77,15 @@ exports.processEvent =  event => {
 
   getConfig()
   .then(config => {
-    // execute the command
-    return exec(cmd, config)
+    return updateGitHubStatus ? updateGitHubStatus(config) : Promise.resolve(config)
+  })
+  .then(config => {
+    if (cmd) {
+      // execute the command
+      return exec(cmd, config)
+    } else {
+      return 'No action required'
+    }
   })
   .catch(err => {
     error(err)

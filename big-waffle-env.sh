07b9bb89a98a7d38337fcb59531d4f270e2987d9
Robert Aarts <robert@lokkilok.com>
2019-12-09 09:43:00 +0000

    Minor improvements.

--- big-waffle-env.sh
+++ big-waffle-env.sh
@@ -2,3 +2,5 @@
 # in the Gapminder BigWaffle deployment on Google Cloud
 export ASSET_STORE_BUCKET="org-gapminder-big-waffle-assets"
 export BIG_WAFFLE_HOME=/gapminders/big-waffle
+export EXTERNAL_LOG='./log-google'
+
